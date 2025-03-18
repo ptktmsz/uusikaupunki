81ba4ca419fbb29417f3b022b3decbde0e29c9fd
TOMPI <TOMPI@orsted.com>
2025-03-18 14:28:54 +0100

    add pandas and first api pull

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
+import pandas as pd
+import requests
+
 def main():
-    print("Hello from uusikaupunki!")
+    date = "2025-03-17"
+    train = "27"
+    url = f"https://rata.digitraffic.fi/api/v1/trains/{date}/{train}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.DataFrame(data)
+        print(df)
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
