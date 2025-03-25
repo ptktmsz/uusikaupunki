97412a95a65d6b80bbe6f6572c2c12d3c77c88cf
TOMPI <TOMPI@orsted.com>
2025-03-25 14:17:32 +0100

    ingestion script with arg parsing and backfilling

--- etl/ingest.py
+++ etl/ingest.py
@@ -1,5 +1,7 @@
+import argparse
+from datetime import datetime, timedelta
 import duckdb
-from etl.client import DigitrafficClient
+from client import DigitrafficClient
 import polars as pl
 
 def ingest_timetable(date: str, train: str) -> None:
@@ -18,4 +20,19 @@ def ingest_timetable(date: str, train: str) -> None:
             """
         )
 
-ingest_timetable("2025-03-23", "27")
+if __name__ == '__main__':
+    yesterday = datetime.today() - timedelta(days=1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train", help="Train ID")
+    parser.add_argument("--date", help="Date to ingest in YYYY-MM-DD  (default: yesterday)", default=yesterday.strftime("%Y-%m-%d"))
+    parser.add_argument("--backfill", help="Backfill mode to ingest data from specified date until yesterday (default: False)", action="store_true")
+    args = parser.parse_args()
+    if args.backfill and args.date == yesterday:
+        parser.error("--date is required when using --backfill")
+    elif args.backfill:
+        processed_date = datetime.strptime(args.date, '%Y-%m-%d')
+        while processed_date < yesterday:
+            ingest_timetable(processed_date.strftime("%Y-%m-%d"), args.train)
+            processed_date += timedelta(days=1)
+    else:
+        ingest_timetable(args.date, args.train)
